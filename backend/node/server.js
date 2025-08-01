bec922ea818c2f1f0947b655ce071b56bed59eef
DharaniManchala <dharanimanchala48@gmail.com>
2025-08-01 10:21:13 +0530

    Update server.js

--- backend/node/server.js
+++ backend/node/server.js
@@ -22,8 +22,10 @@ app.use(cors({
   allowedHeaders: ["Content-Type"],
   credentials: true
 }));
+app.options('*', cors()); /
 
 app.use(express.json());
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
